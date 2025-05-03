ebe06c2fa559370c94d92800702ae4829c287768
Valentina <valyaGau6@mail.ru>
2025-05-03 14:12:32 +0300

    без JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
+// декларативный
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// императивный
 export const App = () => {
-	const year = new Date().getFullYear();
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload2.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div>{year}</div>
-			</header>
-		</div>
-	);
+  const year = new Date().getFullYear();
+  return (
+    // декларативный
+    createElement("div", {
+      className: "App"
+    },
+	createElement("header", {
+      className: "App-header"
+    },
+	createElement("img", {
+      src: logo,
+      className: "App-logo",
+      alt: "logo"
+    }),
+	createElement("p", null, "Edit ", createElement("code", null, "src/App.js"), " and save to reload2."),createElement("a", {
+      className: "App-link",
+      href: "https://reactjs.org",
+      target: "_blank",
+      rel: "noopener noreferrer"
+    }, "Learn React"),
+	createElement("div", null, year)))
+  );
 };
 
 
